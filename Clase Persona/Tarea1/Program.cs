bff9d5adb4b7c70c1bdd4a7a97d1e98effe03931
danrequiem <DAN@iMac-de-Daniel.local>
2013-09-19 12:44:30 -0500

    Se le da valor a las variables de un nuevo objeto y se les llama para imprimirlas en pantalla

--- Clase Persona/Tarea1/Program.cs
+++ Clase Persona/Tarea1/Program.cs
@@ -6,7 +6,27 @@ namespace Tarea1
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			Console.WriteLine("Bienvenido \n");
+
+			Persona P =new Persona();
+
+			Console.Write("Nombre:");
+			P.nombre = Console.ReadLine();
+
+			Console.Write("Correo:");
+			P.correo = Console.ReadLine();
+
+			Console.Write("Edad:");
+			P.edad = int.Parse(Console.ReadLine());
+
+			Console.Write("\nLos datos ingresados son estos \n");
+			Console.Write("\nNombre:"+P.nombre);
+			Console.Write("\nCorreo:"+P.correo);
+			Console.Write("\nEdad:"+P.edad );
+
+
+			Console.Write("\n\nPress any key to continue . . . ");
+			Console.ReadKey(true);
 		}
 	}
 }
